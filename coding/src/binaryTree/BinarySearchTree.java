e5bb5254d78c9b7499e68eb0543c2d1125427b73
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-01-13 15:53:29 -0600

    Cleaned binary tree. Added pre-order, in-order and post-order traverse a BST.

---
+++ coding/src/binaryTree/BinarySearchTree.java
@@ -0,0 +1,130 @@
+package binaryTree;
+
+public class BinarySearchTree {
+//  public Node root;
+  public static void main(String[] args){
+    Node root = new Node(5);
+    System.out.println("Building tree with root value " + root.value);
+    insert(root, 1);
+    insert(root, 8);
+    insert(root, 6);
+    insert(root, 3);
+    insert(root, 9);
+    System.out.println("In-Order Traversal Recursive Solution");
+    inOrderTraversalRecursive(root);
+    System.out.println();
+    System.out.println("In-Order Traversal Iterative solution");
+    inOrderTraversalIterative(root);
+    System.out.println("Pre-Order Traversal Recursive Solution");
+    preOrderTraversalRec(root);
+    System.out.println();
+    System.out.println("Post-Order Traversal Recursive Solution");
+    postOrderTraverseRec(root);
+    System.out.println();
+    System.out.println("Find Minimum");
+    System.out.println(findMinimum(root));
+    System.out.println("Traversing tree front-to-back from location 7");
+    printFrontToBack(root, 7);
+    System.out.println(heightOfBinaryTree(root));
+  }
+  
+  public static void insert(Node node, int value){
+    if(value < node.value){
+      if(node.left != null){
+        insert(node.left, value);
+      }else{
+        node.left = new Node(value);
+      }
+    }else if(value > node.value){
+      if(node.right != null){
+        insert(node.right, value);
+      }else{
+        node.right = new Node(value);
+      }
+    }else{
+      System.out.println("There exists a node that has the same value.");
+    }
+  }
+  
+  public static int findMinimum(Node node){
+    if (node == null) {
+      return 0;
+    }
+    Node currNode = node;
+    while(currNode.left != null){
+      currNode = currNode.left;
+    }
+    return currNode.value;
+  }
+  
+  public static int findMaxium(Node node){
+    if (node == null) {
+      return 0;
+    }
+    Node currNode = node;
+    while(currNode.right != null){
+      currNode = currNode.right;
+    }
+    return currNode.value;
+  }
+  
+  /*In-Order traverse the tree in recursive way*/
+  public static void inOrderTraversalRecursive(Node node) {
+    if (node != null) {
+      inOrderTraversalRecursive(node.left);
+      System.out.print(node.value);
+      inOrderTraversalRecursive(node.right);
+    }
+  }
+  
+  /*In-Order traverse the tree in iterative way*/
+  public static void inOrderTraversalIterative(Node node) {
+
+  }
+  
+  /*Pre-Order traverse the tree in recursive way*/
+  public static void preOrderTraversalRec(Node node){
+    if (node != null) {
+      System.out.print(node.value);
+      preOrderTraversalRec(node.left);
+      preOrderTraversalRec(node.right);
+    }
+  }
+  
+  /*Post-Order traverse the tree in recursive way*/
+  public static void postOrderTraverseRec(Node node) {
+    if (node != null) {
+      postOrderTraverseRec(node.left);
+      postOrderTraverseRec(node.right);
+      System.out.print(node.value);
+    }
+  }
+
+  public static void printFrontToBack(Node node, int camera) {
+    if (node == null)
+      return;
+    if (node.value > camera) {
+      // print in order
+      printFrontToBack(node.left, camera);
+      System.out.println("  Traversed " + node.value);
+      printFrontToBack(node.right, camera);
+    } else if (node.value < camera) {
+      // print reverse order
+      printFrontToBack(node.right, camera);
+      System.out.println("  Traversed " + node.value);
+      printFrontToBack(node.left, camera);
+    } else {
+      // order doesn't matter
+      printFrontToBack(node.left, camera);
+      printFrontToBack(node.right, camera);
+    }
+  }
+  public static int heightOfBinaryTree(Node node){
+    if(node == null){
+      return 0;
+    }else{
+      return 1 + Math.max(heightOfBinaryTree(node.left), heightOfBinaryTree(node.right));
+    }
+  }
+
+}
---
+++ coding/src/binaryTree/Node.java
@@ -0,0 +1,10 @@
+package binaryTree;
+
+public class Node {
+  public int value;
+  public Node left;
+  public Node right;
+  public Node(int value) {
+    this.value = value;
+  }
+}
