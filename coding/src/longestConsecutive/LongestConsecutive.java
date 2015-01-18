1d8970425fe924c3bd1e69e9186ed8e1aa8cace9
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-01-17 21:56:53 -0600

    Added comments to LongestConsecutive.

--- coding/src/longestConsecutive/LongestConsecutive.java
+++ coding/src/longestConsecutive/LongestConsecutive.java
@@ -1,5 +1,17 @@
 package longestConsecutive;
 import java.util.HashSet;
+
+/*
+ * Problem:
+ * Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+ * For example, given [100, 4, 200, 1, 3, 2], the longest consecutive elements sequence should be [1, 2, 3, 4]. Its length is 4.
+ * Your algorithm should run in O(n) complexity.
+ *
+ * Solution:
+ * Because it requires O(n) complexity, we can not solve the problem by sorting the array first. Sorting takes at least O(nlogn) time.
+ * After an element is checked, it should be removed from the set. Otherwise, time complexity would be O(mn) in which m is the average length of all consecutive sequences.
+ * An extreme case here: If n is number of elements, m is average length of consecutive sequence, and m==n, then the time complexity is O(n^2). The reason is that in this case, no element is removed from the set each time. You can use this array to get the point: {1,3,5,7,9}.
+ */
 public class LongestConsecutive {
   public static void main(String[] args) {
     int [] a = {1,4,3,8,2,9,10,13,11,12};
