93697a77d2155d4731b8a511e559e746be8d0314
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-01-24 14:09:24 -0600

    Fixed another conflict before revert

--- clare/AtFirstSight.java
+++
@@ -1,9 +0,0 @@
-package clare;
-
-class AtFirstSight {
-
-	public static void main(String[] args) {
-		System.out.printf("Can I kiss you? I wanna kiss you.");
-	}
-
-}
--- two-sum/index.md
+++
@@ -1,5 +0,0 @@
----
-layout: solution
-title: Two Sum
-date: 2015-01-24 12:44:00
----
