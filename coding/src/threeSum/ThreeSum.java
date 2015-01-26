5282f1dd7eb41fe1416855bd34b87d86302b97de
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-01-25 20:20:33 -0600

    Added a comment to three sum.

--- coding/src/threeSum/ThreeSum.java
+++ coding/src/threeSum/ThreeSum.java
@@ -33,7 +33,7 @@ public class ThreeSum {
     Arrays.sort(num);
     
     for (int i=0; i < num.length - 2; i++) {
-      if (i==0 || num[i] > num[i-1]) {
+      if (i==0 || num[i] > num[i-1]) { // prevent duplicate set
         int mid = i + 1;
         int end = num.length - 1;
         
