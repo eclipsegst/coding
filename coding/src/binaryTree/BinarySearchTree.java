838a75aa99622e22718f3f35b0063dbab34f12d3
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-01-25 22:13:12 -0600

    Added reverse integer. Fixed a warning in binary serach tree.

--- coding/src/binaryTree/BinarySearchTree.java
+++ coding/src/binaryTree/BinarySearchTree.java
@@ -1,7 +1,6 @@
 package binaryTree;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Stack;
 
 public class BinarySearchTree {
---
+++ coding/src/reverse/Solution.java
@@ -0,0 +1,27 @@
+package reverse;
+/*
+ * Date: 01/25/2015 21:56
+ * 
+ * Problem:
+ * Reverse digits of an integer.
+ * For example, x = 123, return 321; x = -123, return -321
+ */
+public class Solution {
+  public static void main(String[] args) {
+    int x = -123;
+    System.out.println(reverse(x));
+  }
+  public static int reverse(int x) {
+    if (x == Integer.MIN_VALUE) return 0;
+    if (x < 0) return -reverse(-x);
+    int y = 0;
+    do {
+      if (y * 10 + x % 10 > Integer.MAX_VALUE) {
+        return 0;
+      }
+      y = y * 10 + x % 10;
+      x /= 10;
+    } while (x > 0);
+    return y;
+  }
+}
