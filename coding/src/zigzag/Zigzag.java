c2ee3d4732f81e6cb06d1fb7258695c4936eafa9
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-01-27 17:41:51 -0600

    Added comments to zigzag.

--- coding/src/zigzag/Zigzag.java
+++ coding/src/zigzag/Zigzag.java
@@ -1,5 +1,21 @@
 package zigzag;
 
+/*
+ * Date: 2015-01-07 01:32:03
+ * 
+ * Problem:
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: 
+ * (you may want to display this pattern in a fixed font for better legibility)
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * And then read line by line: "PAHNAPLSIIGYIR".
+ * Write the code that will take a string and make this conversion given a number of rows:
+ * string convert(string text, int nRows);
+ * convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR"
+ * 
+ */
+
 public class Zigzag {
 	public static void main(String[] args){
 		char m [][] = new char[][]{
