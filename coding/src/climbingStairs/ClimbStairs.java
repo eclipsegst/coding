3e96a9552ecc0d2187830070cccd942c0927b23b
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-01-28 21:28:59 -0600

    Added climbing stairs.

---
+++ coding/src/climbingStairs/ClimbStairs.java
@@ -0,0 +1,31 @@
+/**
+ * Author : Zhaolong Zhong
+ * Date   : Jan 28, 2015 8:48:58 PM
+ *
+ * Problem:
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ * Each time you can either climb 1 or 2 steps. 
+ * In how many distinct ways can you climb to the top? 
+ */
+package climbingStairs;
+
+public class ClimbStairs {
+  public static int climbStairs(int n) {
+    int[] step = new int[Math.max(n + 1, 3)];
+    
+    step[0] = 0;
+    step[1] = 1;
+    step[2] = 2;
+    
+    for(int i = 3; i <= n; i++){
+      step[i] = step[i - 1] + step[i - 2];
+    }
+    
+    return step[n];
+  }
+  
+  public static void main(String[] args) {
+    int n = 4;
+    System.out.println(climbStairs(n));
+  }
+}
