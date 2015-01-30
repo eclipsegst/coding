b5443a018d106491d47b46ab0a68d21654dde5d0
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-01-29 21:58:54 -0600

    Added pascal's triangle two.

---
+++ coding/src/pascalTriangle/PascalTriangleTwo.java
@@ -0,0 +1,37 @@
+/**
+ * Author : Zhaolong Zhong
+ * Date   : Jan 29, 2015 9:37:43 PM
+ * 
+ * Problem:
+ * Given an index k, return the kth row of the Pascal's triangle.
+ * For example, given k = 3,
+ * Return [1,3,3,1]
+ * Note: Could you optimize your algorithm to use only O(k) extra space.
+ */
+package pascalTriangle;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+public class PascalTriangleTwo {
+  public static List<Integer> getRow(int rowIndex) {
+    List<Integer> res = new ArrayList<Integer>();
+    Integer[] prev = null;
+    for(int i = 1; i <= rowIndex + 1; i++){
+      Integer[] row = new Integer[i];
+      row[0] = 1;
+      row[i - 1] = 1;
+      for(int j = 1; j < i - 1; j++){
+        row[j] = prev[j - 1] + prev[j];
+      }
+      prev = row;
+      res = Arrays.asList(row);
+    }
+    return res;
+  }
+  public static void main(String[] args) {
+    List<Integer> list = new ArrayList<Integer>();
+    list = getRow(3);
+    System.out.println(Arrays.toString(list.toArray()));
+  }
+}
