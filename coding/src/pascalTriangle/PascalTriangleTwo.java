fe52de057755d91348e3bcbefc5ad9e0b40afbf8
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-01-29 22:12:01 -0600

    Added an alternative approach to pascal triangle two.

--- coding/src/pascalTriangle/PascalTriangleTwo.java
+++ coding/src/pascalTriangle/PascalTriangleTwo.java
@@ -29,9 +29,22 @@ public class PascalTriangleTwo {
     }
     return res;
   }
+  
+  public List<Integer> getRowTwo(int rowIndex) {
+    Integer[] row = new Integer[rowIndex + 1];
+    Arrays.fill(row, 1);
+    for(int i = 0; i < rowIndex - 1; i++){
+      for(int j = i + 1; j >= 1; j--){
+        row[j] = row[j] + row[j - 1];
+      }
+    }
+    return Arrays.asList(row);
+  }
+  
   public static void main(String[] args) {
     List<Integer> list = new ArrayList<Integer>();
     list = getRow(3);
     System.out.println(Arrays.toString(list.toArray()));
   }
+  
 }
