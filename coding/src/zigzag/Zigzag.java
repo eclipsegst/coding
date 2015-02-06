fe4e6de947685617123e668c99eb389115667d54
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-02-06 00:05:23 -0600

    Cleaned up the zig-zag code.

--- coding/src/zigzag/Zigzag.java
+++ coding/src/zigzag/Zigzag.java
@@ -4,65 +4,49 @@ package zigzag;
  * Date: 2015-01-07 01:32:03
  * 
  * Problem:
- * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: 
- * (you may want to display this pattern in a fixed font for better legibility)
- * P   A   H   N
- * A P L S I I G
- * Y   I   R
- * And then read line by line: "PAHNAPLSIIGYIR".
- * Write the code that will take a string and make this conversion given a number of rows:
- * string convert(string text, int nRows);
- * convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR"
+ * Given a two dimension array, zig-zag print it, return the string with zig-zag print order. 
+ * For example,
+ * char c [][] = new char[][]{
+ *   {'a', 'b', 'c'},
+ *   {'d', 'e', 'f'},
+ *   {'g', 'h', 'i'},
+ *   {'j', 'k', 'l'}
+ * };
+ * 
+ * return "adbcegjhfikl"
  * 
  */
 
 public class Zigzag {
-	public static void main(String[] args){
-		char m [][] = new char[][]{
-				{'a', 'b', 'c'},
-				{'d', 'e', 'f'},
-				{'g', 'h', 'i'},
-				{'j', 'k', 'l'}
-		};
-		
-		int height = m.length;
-		int width =m[0].length;
-		System.out.println(height);
-		System.out.println(width);
-		
-		int i =0, j = 0;
-
-		while(!((i==height-1) && (j==width-1)) && (i<height -1 && j<width-1)){	
-			
-			System.out.println(m[i][j]);
-			if(i<height-1){
-				i++;//go down in left edge
-			}else if(j<width-1){
-				j++;//go right in bottom edge
-			}else{
-				break;
-			}
-			//go right top
-			while(i>0 && j<width-1){
-				System.out.println(m[i][j]);
-				i--;
-				j++;
-			}
-			
-			System.out.println(m[i][j]);//top edge
-			
-			if(i==0 && j<width-1){
-				j++;//go right on top edge
-			}else
-			{
-				i++;//go down on right edge
-			}
-			// go down left
-			while(j>0 && i<height-1){
-				System.out.println(m[i][j]);
-				j--;
-				i++;
-			}
-		}
-	}
+  public static String zigZag(char[][] c) {
+    String res = "";
+    int row = c.length;
+    int col = c[0].length;
+    int i = 0, j = 0;
+    while(!(i == row- 1 && j == col - 1)) {
+      res += c[i][j];
+      if(i < row - 1){
+        i++;
+      } else if(j < col - 1){
+        j++;
+      }
+      while(i > 0 && j < col - 1){
+        res += c[i][j];
+        i--;
+        j++;
+      }
+      res += c[i][j];
+      if(j < col -1){
+        j++;
+      } else if(i < row -1){
+        i++;
+      }
+      while(j > 0 && i < row - 1){
+        res += c[i][j];
+        i++;
+        j--;
+      }
+    }
+    return res;
+  }
 }
