b05fd96072c7cd61a2f9a092b43aa537998597ad
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-03-12 23:06:29 -0500

    add abstract class

---
+++ coding/basic/Abstract/Circle.java
@@ -0,0 +1,15 @@
+/**
+ * Author: Zhaolong Zhong
+ * Date  : Mar 12, 2015 10:50:02 PM
+ *
+ */
+package Abstract;
+
+public class Circle extends GraphicObject {
+  void draw() {
+    
+  }
+  void resize() {
+    
+  }
+}
---
+++ coding/basic/Abstract/Rectangle.java
@@ -0,0 +1,15 @@
+/**
+ * Author: Zhaolong Zhong
+ * Date  : Mar 12, 2015 10:49:19 PM
+ *
+ */
+package Abstract;
+
+public class Rectangle extends GraphicObject {
+  void draw() {
+    
+  }
+  void resize() {
+    
+  }
+}
