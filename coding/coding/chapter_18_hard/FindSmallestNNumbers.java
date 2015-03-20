3c5f7e23689e3e376cc6d01beecc7931aacfd9a9
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-03-20 01:33:45 -0500

    refactor findKthLargestNumber add explanation/comments to FindSmallestNNumber

--- coding/coding/chapter_18_hard/FindSmallestNNumbers.java
+++ coding/coding/chapter_18_hard/FindSmallestNNumbers.java
@@ -1,11 +1,31 @@
 /**
- * Author: Zhaolong Zhong
- * Date  : Feb 7, 2015 1:33:46 AM
- *
+ *  Author : Zhaolong Zhong
+ *  Date   : Feb 7, 2015 1:33:46 AM
+ *  Problem:
+ *      Describe an algorithm to find the smallest one million numbers(m) in one billion numbers.
+ *      Assume that the computer memory can hold all one billion numbers(n).
+ * 
+ *  Solution 1: Sorting 
+ *      We can sort the elements in asscending order and then take the first one million number from that.
+ *      The time complexity is O(n log(n)).
+ * 
+ *  Solution 2: Min Heap/PriorityQueue
+ *      We can use min heap to solve this problem.
+ *      Since PriorityQueue providesO(log(n)) time for the enqueing and dequeing methods(offer, poll, remove(), and add),
+ *      and constant time for the retrieve methods(peek, element, and size). 
+ *      So we are going to have m size PriorityQueue, the operation time each element is O(log m).
+ *      The time complexity is O(n log(m)).
+ *  
+ *  Solution 3: Selection Rank
+ *      Selection Rank is well-known algorithm(medium of medium or quick select) in computer science to find the ith smallest 
+ *      (or largest) element in an array in linear time.
+ *      If the elements are unique, you can find the ith smallest element in expected O(n) time.
+ *      Once we have found the ith smallest element, we can run through the array to find all values less than or equal to this element.
+ *  
  */
-package findKthLargestNumber;
+package chapter_18_hard;
 
-public class FindKthLargestNumber {
+public class FindSmallestNNumbers {
   public static void main(String[] args) {
     int[] nums = new int[20];
     java.util.Random myRandom = new java.util.Random();
@@ -40,7 +60,9 @@ public class FindKthLargestNumber {
         swap(nums, left, right);
       }
     }
+    
     swap(nums, pivot, right);
+    
     if(k == right + 1) {
       return nums[right];
     } else if (k > right + 1) {
