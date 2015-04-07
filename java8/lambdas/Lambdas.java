8ce3c2395b779302cb8429e73682677ffab49602
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-04-07 18:20:48 -0500

    Added Letter Combinations of a Phone Number.

--- java8/lambdas/Lambdas.java
+++ java8/lambdas/Lambdas.java
@@ -9,29 +9,37 @@ import java.util.Arrays;
 
 public class Lambdas {
   public static void main(String[] args) {
+    System.out.println("Case 1");
     Arrays.asList("a", "b", "d").forEach( e -> System.out.println(e));
     
+    System.out.println("Case 2");
     // provide the type of the argument e
     Arrays.asList("a", "b", "d").forEach((String e) -> System.out.println(e));
     
+    System.out.println("Case 3");
     // wrap lambda's body into square brackets if it's too complex
     Arrays.asList("a", "b", "d").forEach( e -> {
       System.out.print(e);
       System.out.print(e);
     });
     
+    System.out.println("Case 5");
     // lambdas may reference the class members and local variables(implicitly making them effectively final if they are not.)
     // the two below are equivalent.
     String separator = ",";
     Arrays.asList("a", "b", "d").forEach(
         (String e) -> System.out.print(e + separator));
     
+    System.out.println("Case 6");
     final String separatorFinal = ",";
     Arrays.asList("a", "b", "d").forEach(
         (String e) -> System.out.print(e + separatorFinal));
     
     // lambdas may return a value. The return statement is not required if the lambda body is just a one-liner.
+    System.out.println("Case 7");
     Arrays.asList("c", "l", "a", "r", "e").sort( (e1, e2) -> e1.compareTo(e2) );
+    
+    System.out.println("Case 8");
     Arrays.asList("c", "l", "a", "r", "e").sort( (e1, e2) -> {
       int result = e1.compareTo(e2);
       return result;
