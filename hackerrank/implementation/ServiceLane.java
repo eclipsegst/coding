72fad51c58dd67f020e60c5ba3a5a561e0138648
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-08-13 17:49:46 -0500

    add ServiceLane

---
+++ hackerrank/implementation/ServiceLane.java
@@ -0,0 +1,32 @@
+package implementation;
+
+import java.util.*;
+
+public class ServiceLane {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int n = in.nextInt();
+		int t = in.nextInt();
+		
+		int[] nums = new int[n];
+		for (int i = 0; i < n; i++) {
+			nums[i] = in.nextInt();
+		}
+		
+		Arrays.asList(nums).forEach(e -> System.out.print(e + " "));
+		
+		for (int j = 0; j < t; j++) {
+			int s = in.nextInt();
+			int e = in.nextInt();
+			int min = 3;
+			for (int k = s; k <= e; k++) {
+				if (min > nums[k]) min = nums[k];
+			}
+			System.out.println(min);
+		}
+		
+		in.close();
+	}
+}
