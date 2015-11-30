56a43ab21bc24dc951d893389533a0462fbb96b9
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-11-30 16:36:19 -0600

    added canPermutePalindrome

--- leetcode/leetcode/MainTest.java
+++ leetcode/leetcode/MainTest.java
@@ -17,5 +17,19 @@ public class MainTest {
 		assertEquals("testNumArray", -1, numArray.sumRange(2, 5));
 		assertEquals("testNumArray", -3, numArray.sumRange(0, 5));
 	}
+	
+	@Test
+	public void testPalindrome() {
+		Palindrome solution = new Palindrome();
+		String s0 = "code";
+		String s1 = "aab";
+		String s2 = "carerac";
+		assertEquals("test Palindrome", false, solution.canPermutePalindrome(s0));
+		assertEquals("test Palindrome", true, solution.canPermutePalindrome(s1));
+		assertEquals("test Palindrome", true, solution.canPermutePalindrome(s2));
+		assertEquals("test Palindrome", false, solution.canPermutePalindromeTwo(s0));
+		assertEquals("test Palindrome", true, solution.canPermutePalindromeTwo(s1));
+		assertEquals("test Palindrome", true, solution.canPermutePalindromeTwo(s2));
+	}
 
 }
