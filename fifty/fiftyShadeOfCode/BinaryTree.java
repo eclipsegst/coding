9fe4d2980cdd58630a644ae602419102ef4a3558
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-12-10 10:34:37 -0600

    improved binary tree

--- fifty/fiftyShadeOfCode/BinaryTree.java
+++ fifty/fiftyShadeOfCode/BinaryTree.java
@@ -5,6 +5,8 @@ package fiftyShadeOfCode;
  */
 
 public class BinaryTree {
+	
+	// O(n^2) runtime - brute force
 	public boolean isValidBST(TreeNode root) {
 		if(root == null) return true;
 		return isSubtreeLessThan(root.left, root.val)
@@ -24,5 +26,34 @@ public class BinaryTree {
 		return p.val > val
 				&& isSubtreeGreaterThan(p.left, val)
 				&& isSubtreeGreaterThan(p.right, val);
+	}
+	
+	// O(n) runtime, O(n) stack space - Top-down recursion:
+	public boolean isValidBSTRecursion(TreeNode root) {
+		return valid(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+	
+	private boolean valid(TreeNode p, int low, int high) {
+		if (p == null) return true;
+		return p.val > low && p.val < high
+				&& valid(p.left, low, p.val)
+				&& valid(p.right, p.val, high);
+	}
+	
+	// O(n) runtime, O(n) stack space - In-order traversal
+	private TreeNode pre;
+	public boolean isValidBSTInOrder(TreeNode root) {
+		pre = null;
+		return isMonotonicIncreasing(root);
+	}
+	
+	private boolean isMonotonicIncreasing(TreeNode p) {
+		if (p == null) return true;
+		if (isMonotonicIncreasing(p.left)) {
+			if (pre != null && p.val <= pre.val) return false;
+			pre = p;
+			return isMonotonicIncreasing(p.right);
+		}
+		return false;
 	}
 }
