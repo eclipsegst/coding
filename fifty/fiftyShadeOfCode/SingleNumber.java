37a88eaeda4e9ecd0968417104838ea4aea1e4f9
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-12-15 22:41:29 -0600

    added single number

---
+++ fifty/fiftyShadeOfCode/SingleNumber.java
@@ -0,0 +1,50 @@
+package fiftyShadeOfCode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Zhaolong Zhong Dec 15, 2015
+ */
+public class SingleNumber {
+	
+	// O(n) runtime - hash map
+	public int singleNumber(int[] nums) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int n : nums) {
+			int count = map.containsKey(n) ? map.get(n) : 0;
+			map.put(n, count + 1);
+		}
+		
+		for (int n : nums) {
+			if (map.get(n) == 1) {
+				return n;
+			}
+		}
+		throw new IllegalArgumentException("no single element");
+	}
+	
+	// O(n) runtime - hash set
+	public int singleNumberBetter(int[] nums) {
+		Set<Integer> set = new HashSet<>();
+		for (int n : nums) {
+			if (set.contains(n)) {
+				set.remove(n);
+			} else {
+				set.add(n);
+			}
+		}
+		return set.iterator().next();
+	}
+	
+	// xor
+	public int singleNumberBit(int[] nums) {
+		int num = 0;
+		for (int n : nums) {
+			num ^= n;
+		}
+		return num;
+	}
+}
