b17883c49321e62b3af740dce9d7ad8ca0f94e56
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-12-20 19:19:34 -0600

    added single number not appear three times

--- fifty/fiftyShadeOfCode/SingleNumber.java
+++ fifty/fiftyShadeOfCode/SingleNumber.java
@@ -47,4 +47,17 @@ public class SingleNumber {
 		}
 		return num;
 	}
+	
+	// single number not appear three times
+	public int singleNumberThree(int nums[], int n) {
+		int ones = 0, twos = 0, threes = 0;
+		for (int i = 0; i < nums.length; i++) {
+			twos |= ones & nums[i];
+			ones ^= nums[i];
+			threes = ones & twos;
+			ones &= ~threes;
+			twos &= ~threes;
+		}
+		return ones;
+	}
 }
