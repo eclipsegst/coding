517896c94d7d163700115fb5b012ccdc079f698b
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-12-25 17:19:44 -0600

    added PeakElement

---
+++ leetcode/g/PeakElement.java
@@ -0,0 +1,23 @@
+package g;
+
+/**
+ * @author Zhaolong Zhong Dec 25, 2015
+ */
+public class PeakElement {
+	// O(lg n) is something like binary search or merge sort
+	public int findPeakElement(int[] num) {
+		return findPeakElement(num, 0, num.length);
+	}
+	
+	private int findPeakElement(int[] num, int from, int to) {
+		if (to - from == 1) return from;
+		
+		int m = (to + from) / 2;
+		
+		int l = findPeakElement(num, from, m);
+		int r = findPeakElement(num, m, to);
+		
+		if (num[l] > num[r]) return l;
+		else return r;
+	}
+}
