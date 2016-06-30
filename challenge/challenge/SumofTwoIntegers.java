9feea400b8fd3a9715698d1a0219add1114bc89a
Zhaolong Zhong <zztg2@mail.missouri.edu>
2016-06-30 00:24:20 -0500

    added recursive way of SumofTwoIntegers

--- challenge/challenge/SumofTwoIntegers.java
+++ challenge/challenge/SumofTwoIntegers.java
@@ -10,6 +10,9 @@ package challenge;
  * Example:
  * Given a = 1 and b = 2, return 3.
  * 
+ * Reference:
+ * https://en.wikipedia.org/wiki/Two%27s_complement
+ * https://www.cs.cornell.edu/~tomf/notes/cps104/twoscomp.html
  */
 public class SumofTwoIntegers {
 	public int getSum(int a, int b) {
@@ -23,22 +26,46 @@ public class SumofTwoIntegers {
 		
 		while (b != 0) {
 			int carry = a & b;
-			
 			a = a ^ b;
-			
 			b = carry << 1;
 		}
 		
 		return a;
     }
 	
+	public int getSubtract(int a, int b) {
+		while (b != 0) {
+			int borrow = (~a) & b;
+			a = a ^ b;
+			b = borrow << 1;
+		}
+		
+		return a;
+	}
+	
+	// Recursive
+	public int getSumRecursive(int a, int b) {
+		return (b == 0) ? a : getSumRecursive(a ^ b, (a & b) << 1);
+	}
+	// Recursive
+	public int getSubtractRecursive(int a, int b) {
+		return (b == 0) ? a : getSubtractRecursive(a ^ b, (~a & b) << 1);
+	}
+	
+	// Get negative number
+	public int negate(int x) {
+		return getSumRecursive(~x, 1);
+	}
+	
 	public static void main(String[] args) {
 		SumofTwoIntegers solution = new SumofTwoIntegers();
 		
 		int a = 1, b = 2;
+		int sum = solution.getSum(a, b);
+		int substract = solution.getSubtract(a, b);
 		
-		int result = solution.getSum(a, b);
-		
-		System.out.print(result);
+		System.out.println(sum);
+		System.out.println(substract);
+		System.out.println(solution.negate(2));
 	}
 }
